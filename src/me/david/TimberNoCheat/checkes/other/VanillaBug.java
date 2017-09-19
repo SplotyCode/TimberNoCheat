867699d36842eb0535bfd14a359f5d286ac89dd7
David_369 <davidscandurra@gmail.com>
2017-09-19 23:44:30 +0200

    Fix ValillaBug false positions

--- src/me/david/TimberNoCheat/checkes/other/VanillaBug.java
+++ src/me/david/TimberNoCheat/checkes/other/VanillaBug.java
@@ -33,7 +33,7 @@ public class VanillaBug extends Check {
     @EventHandler
     public void onInteract(PlayerInteractEvent e){
         //System.out.println(!fences.contains(e.getClickedBlock().getType()));
-        if (!TimberNoCheat.checkmanager.isvalid_create(e.getPlayer()) || e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getItem() == null || !fences.contains(e.getClickedBlock().getType()) && !e.getItem().getType().isBlock()) {
+        if (!TimberNoCheat.checkmanager.isvalid_create(e.getPlayer()) || e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getItem() == null || !fences.contains(e.getClickedBlock().getType()) || !e.getItem().getType().isBlock()) {
             return;
         }
         e.setCancelled(true);
